6e8be69f5f18e21c1d085668e0840792140ec9b5
João Paulo <joao.06.paulo9@gmail.com>
2024-05-19 18:11:46 -0300

    feat: add status 400 for existing user

--- src/controllers/UserControlller.ts
+++ src/controllers/UserControlller.ts
@@ -5,7 +5,13 @@ export class UserController {
     async register(request: Request, response: Response) {
         const { email, password } = request.body;
 
-        const user = await prismaClient.user.create({
+        const userExists = await prismaClient.user.findFirst({ where: {email} })
+
+        if(userExists) {
+            return response.status(400).json({ status: 400, message: 'Usuário já existente.' })
+        } 
+
+        const user = await prismaClient.user.create({   
             data: {
                 email,
                 password
