c20baefdf0747862c2a1515f62d7a88800fb7c42
João Paulo <joao.06.paulo9@gmail.com>
2024-05-19 18:10:11 -0300

    feat: add post, patch and delete routes

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,10 @@ const router = Router();
 const task = new TaskController();
 const user = new UserController();
 
-router.post('/task', task.createTask);
 router.get('/tasks-list', task.findAllTasks);
+router.post('/task', task.createTask);
+router.patch('/task/:id', task.updateTask);
+router.delete('/delete-task/:id', task.deleteTask);
 
 router.post('/register', user.register)
 router.post('/login')
