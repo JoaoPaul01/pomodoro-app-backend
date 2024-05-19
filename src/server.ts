062c3887fe51e487ef13eae0f0e9ede7d4613b67
João Paulo <joao.06.paulo9@gmail.com>
2024-05-19 18:10:47 -0300

    feat: add swagger route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+
 import { router } from './routes';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 const port = 3000;
 const app = express();
 
-
 app.use(express.json());
-app.use(router)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+app.use('/terms', (request, response) => {
+    return response.json({
+        message: 'Termos de uso' 
+    });
+})
+
+app.use('/v1', router)
 
 app.listen(port, () => console.log(`Servidor inicializado em http://localhost:${port}`));
