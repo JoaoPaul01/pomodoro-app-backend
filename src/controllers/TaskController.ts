3b91d29fa081f310e83d66cc91e72088e41cf497
João Paulo <joao.06.paulo9@gmail.com>
2024-05-19 18:09:56 -0300

    feat: add update and delete controllers

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -8,16 +8,58 @@ export class TaskController {
     };
 
     async createTask(request: Request, response: Response) {
-        const { name, turn } = request.body;
+        const { name, description ,turn } = request.body;
 
         const task = await prismaClient.task.create({
             data: {
                 name,
+                description,
                 turn,
                 status: true
             }
         });
 
         return response.json(task)
+    }
+
+    async updateTask(request: Request, response: Response) {
+        const { id } = request.params;
+        const { name, description, turn, status } = request.body;
+
+        const taskExists = await prismaClient.user.findFirst({ where: {id} })
+
+        if(!taskExists) {
+            return response.status(400).json({ status: 400, message: 'Task does not exists.' })
+        }
+        
+        await prismaClient.task.update({
+            where: { id: Number(id) },
+            data: {
+                name,
+                description,
+                turn,
+                status
+            }
+        });
+
+        const tasks = await prismaClient.task.findMany();
+        return response.json(tasks);
+    }
+
+    async deleteTask(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const taskExists = await prismaClient.user.findFirst({ where: {id} })
+
+        if(!taskExists) {
+            return response.status(400).json({ status: 400, message: 'Task does not exists.' })
+        }
+
+        await prismaClient.task.delete({
+            where: { id: Number(id) }
+        });
+
+        const tasks = await prismaClient.task.findMany();
+        return response.json(tasks);
     }
 }
