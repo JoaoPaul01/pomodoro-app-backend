ec22e20bf109c2f957bdbb09b5c561093c89044a
João Paulo <joao.06.paulo9@gmail.com>
2024-05-19 15:22:51 -0300

    chance js to ts

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { router } from './routes';
+
+const port = 3000;
+const app = express();
+
+
+app.use(express.json());
+app.use(router)
+
+app.listen(port, () => console.log(`Servidor inicializado em http://localhost:${port}`));
